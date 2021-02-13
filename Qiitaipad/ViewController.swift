68fb531c752feaa34d2c317eddaf160c7957abdb
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-13 20:17:44 +0900

    Initial Commit

---
+++ Qiitaipad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qiitaipad
+//
+//  Created by Takuya Ando on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
